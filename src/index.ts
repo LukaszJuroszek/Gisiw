a6f9cbcf90bc0416bebc3df3d38107ddb45d840d
Łukasz Juroszek <lukjuroszek@gmail.com>
2019-05-13 20:12:00 +0200

    Add index styling

--- src/index.ts
+++ src/index.ts
@@ -172,7 +172,7 @@ function generateSumChart(dataPointsOfF1Sum: any[], dataPointsOfF2Sum: any[], da
     var sumChart = new CanvasJS.Chart("sumChart", {
         animationEnabled: true,
         zoomEnabled: true,
-
+        
         axisY: {
             includeZero: false,
         },
@@ -183,37 +183,38 @@ function generateSumChart(dataPointsOfF1Sum: any[], dataPointsOfF2Sum: any[], da
         {
             includeZero: false,
         }],
-        // legend: {
-        //     cursor: "pointer",
-        //     verticalAlign: "top",
-        //     fontSize: 22,
-        //     fontColor: "black",
-        // },
 
-        // toolTip: {
-        //     shared: false
-        // },
+        legend: {
+            cursor: "pointer",
+            verticalAlign: "top",
+            fontSize: 22,
+            fontColor: "black",
+        },
+
+        toolTip: {
+            shared: false
+        },
 
         data: [
             {
                 type: "line",
-                // showInLegend: true,
-                // name: "Sum F1(x) and F2(x)",
+                showInLegend: true,
+                name: "Sum F1(x) and F2(x) of best chromosome",
                 dataPoints: dataPointsOfF1AndF2SumOfBestChromosome
             },
             {
                 type: "line",
                 axisYType: "secondary",
-                // showInLegend: true,
-                // name: "Sum F1(x)",
+                showInLegend: true,
+                name: "Sum F1(x)",
                 dataPoints: dataPointsOfF1Sum
             },
             {
                 type: "line",
                 axisYType: "secondary",
                 axisYIndex: 1,
-                // showInLegend: true,
-                // name: "Sum F2(x)",
+                showInLegend: true,
+                name: "Sum F2(x)",
                 dataPoints: dataPointsOfF2Sum
             }
         ]
