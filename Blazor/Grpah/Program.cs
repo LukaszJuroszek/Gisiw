abd736f479fb304319ed0ed355a173661759bebf
Łukasz Juroszek <lukjuroszek@gmail.com>
2020-05-04 22:36:02 +0200

    Add gitatributes to host blazor wasm on gitpage

--- Blazor/Grpah/Program.cs
+++ Blazor/Grpah/Program.cs
@@ -13,7 +13,9 @@ namespace Graph
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
             builder.RootComponents.Add<App>("app");
+            
             builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddSingleton<IChromosomeService,ChromosomeService>();
             builder.Services.AddSingleton<IEvolutionService, EvolutionService>();
